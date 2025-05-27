6a6da4ce7287e5c3c3e545d5a27c092fd3dd2067
muri <106119896+mur1@users.noreply.github.com>
2025-05-27 10:28:50 -0300

    Delete loader.lua

--- loader.lua
+++
@@ -1,7 +0,0 @@
-local player = game.Players.LocalPlayer
-
-if(game.PlaceId == 9825515356) then
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/mur1/shawty/refs/heads/main/shawtyinternal.lua"))()
-else
-      player:Kick("Game is not supported!")
-end
